e88c2acea6391c6450b883e9c2e305c9423e61c3
Dharmendradeveloper <kumardakdharmendra@gmail.com>
2024-06-16 11:53:24 +0530

    use remeber

---
+++ app/src/main/java/com/example/myjetpack/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.myjetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myjetpack.ui.theme.MyJetpackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    var counter by rememberSaveable {
+        androidx.compose.runtime.mutableIntStateOf(0)
+    }
+
+    val decrementCounter = {
+        counter--
+    }
+
+    val incrementCounter = {
+        counter++
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+        ) {
+            Button(onClick = { incrementCounter.invoke() }, modifier = Modifier.padding(5.dp)) {
+                Text(text = "Increment")
+            }
+            Text(text = "$counter")
+            Button(onClick = { decrementCounter.invoke() }, modifier = Modifier.padding(5.dp)) {
+                Text(text = "Decrement")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Greeting()
+}
